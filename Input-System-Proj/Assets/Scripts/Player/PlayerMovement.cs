b596464587464cca2fcf952f97ced7f35f3a8c84
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-09-05 21:06:01 -0300

    Aula 05/09
    Criação das bases de um Input System para movimentação

---
+++ Input-System-Proj/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterControls playerInputs;
+    private Transform playerTransform;
+    private Vector2 moveDirection;
+
+    [SerializeField] private float velocity;
+
+    private void Awake()
+    {
+        playerTransform = GetComponent<Transform>();
+
+        playerInputs = new CharacterControls();
+        playerInputs.PlayerBehavior.Move.started += OnMoveInputReceived;
+        playerInputs.PlayerBehavior.Move.performed += OnMoveInputReceived;
+        playerInputs.PlayerBehavior.Move.canceled += OnMoveInputReceived;
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        playerTransform.Translate(new Vector2(moveDirection.x, moveDirection.y) * velocity * Time.deltaTime);
+    }
+
+    private void OnMoveInputReceived(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        playerInputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputs.Disable();
+    }
+
+}
