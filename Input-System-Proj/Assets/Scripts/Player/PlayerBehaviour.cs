37e3bd1f525dcceeeaed2596ce75aa09ff32250b
gholyra <tiago.mlyra@gmail.com>
2024-02-04 23:03:17 -0300

    Animation and Control Updates
    - Idle animation e Run Animation
    - Melhorias no PlayerBehaviour

--- Input-System-Proj/Assets/Scripts/Player/PlayerBehaviour.cs
+++ Input-System-Proj/Assets/Scripts/Player/PlayerBehaviour.cs
@@ -7,37 +7,53 @@ public class PlayerBehaviour : MonoBehaviour
     
     private Transform playerTransform;
     private Vector2 moveDirection;
+    private Animator playerAnimator;
+    private SpriteRenderer spriteRenderer;
 
     [SerializeField] private float velocity;
 
     private void Awake()
     {
         playerTransform = GetComponent<Transform>();
+        playerAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         playerInputs = new CharacterControls();
         playerInputs.PlayerBehaviour.Move.started += OnMoveInputReceived;
         playerInputs.PlayerBehaviour.Move.performed += OnMoveInputReceived;
         playerInputs.PlayerBehaviour.Move.canceled += OnMoveInputReceived;
+        
+        playerInputs.PlayerBehaviour.Jump.started += OnJumpInputReceived;
     }
 
     private void Update()
     {
         MovePlayer();
-
-        if (moveDirection.x != 0)
-        {
-            
-        }
     }
 
     private void MovePlayer()
     {
+        if (moveDirection.x != 0)
+        {
+            spriteRenderer.flipX = !(moveDirection.x > 0);
+        }
+        
         playerTransform.Translate(new Vector2(moveDirection.x, moveDirection.y) * velocity * Time.deltaTime);
+    }
+
+    private void HandleAnimation()
+    {
+        
     }
 
     private void OnMoveInputReceived(InputAction.CallbackContext context)
     {
         moveDirection.x = context.ReadValue<float>();
+    }
+
+    private void OnJumpInputReceived(InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
     }
 
     private void OnEnable()
